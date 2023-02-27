4c4db3dfeefc32a1bc8c870d668cb93255a1e3f8
john-almaraz <125034147+john-almaraz@users.noreply.github.com>
2023-02-27 21:44:58 +0100

    ejercicio 4 terminado, ejercio 5 pendiente de solucionar

--- cursoJavaScript/ejercicios/_04_invertir_texto.js
+++ cursoJavaScript/ejercicios/_04_invertir_texto.js
@@ -2,3 +2,34 @@
 4) Programa una función que invierta las palabras de una cadena de texto, 
 miFuncion("Hola Mundo") devolverá "odnuM aloH".
 */
+
+/**
+ * Este metodo invierte una cadena de texto.
+ * 
+ * @param {String} cadena - cadena de texto. No puede ser 
+ * @returns {String} cadena de texto invertida
+ */
+const invertirTexto=(cadena)=>{    
+    if(cadena ===undefined){
+        return 'value undefined'
+    }else if(cadena ===null){
+        return 'value null'
+    }else{
+        let nuevaCadena = cadena.trim()        
+        if(nuevaCadena ===''){
+            return 'string empty'
+        }else if(!isNaN(nuevaCadena)){
+            throw new TypeError("El parametro cadena no puede ser un valor numerico")
+        }else{
+            let textoInvertido = ""
+            let array = Array.from(cadena)
+            for(let i = array.length-1; i > -1;i--){
+                textoInvertido += array[i]
+            }
+            return textoInvertido
+        }
+    }
+    
+}
+let texto = "Este texto sera invertido"
+console.log(invertirTexto(texto))
---
+++ cursoJavaScript/ejercicios/_05_buscar_repetidos.js
@@ -0,0 +1,17 @@
+/*
+5) Programa una funcion para contar el numero de veces que se repite una palabra en un texto largo,  
+miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+*/
+
+/**
+ * Este metodo busca palabras repetidas.
+ * 
+ * @param {String} cadena - cadena que sera evaluada
+ * @param {String} busqueda - palabra o frase que sera buscada
+ * @returns {Number} numero de veces que se repite una palabra o frase
+ */
+const buscarRepetidos=(cadena,busqueda)=>{
+    let count = 0
+    const regex = new RegExp(busqueda,"")
+    return count
+}
