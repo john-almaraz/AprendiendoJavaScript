ecd95ac9ec653a9e139d7f5c10dea73036fbce0a
john-almaraz <125034147+john-almaraz@users.noreply.github.com>
2023-03-08 12:52:04 +0100

    eliminado de un archivo que ya fue renombrado como coca_cola.js

--- cursoJavaScript/ejercicios/_27_coca_cola/supermarket.js
+++
@@ -1,8 +0,0 @@
-const bebidas = document.querySelectorAll('.product-img')
-
-const selectProduct = (evento)=>{
-    let nameProduct = evento.target.title
-    console.log(nameProduct)
-}
-bebidas.forEach((e)=>e.addEventListener('click',selectProduct))
-
